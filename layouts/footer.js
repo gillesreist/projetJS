9f32564e78e4f3d822e9d83f594bb8d72f3e247c
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-07-20 16:25:00 +0200

    terminé la structure css d'accueil footer et header

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,7 +1,7 @@
 const footer = document.createElement('footer');
 footer.innerHTML = `
 <div class="footer_container">
-  <section>
+  <section class="reseaux">
     <div>
       <span>Rejoignez-nous sur les réseaux sociaux :</span>
     </div>
@@ -14,31 +14,27 @@ footer.innerHTML = `
       </a>
     </div>
   </section>
-  <section>
-    <div >
-      <div>
-        <div>
-          <h6 >
-            Lesupersite
-          </h6>
-          <p>
-            Un site qu'il est chouette.
-          </p>
-        </div>
-        <div>
-          <h6>Contact</h6>
-          <p>Valence, 26000, FR</p>
-          <p>
-            <a href="mailto:contact@lesupersite.fr"></a>
-              contact@lesupersite.com
-            </a>
-          </p>
-          <p>06 27 87 45 32</p>
-        </div>
-      </div>
+  <section class="footer_text">
+    <div>
+      <h6 >
+        Lesupersite
+      </h6>
+      <p>
+        Un site qu'il est chouette.
+      </p>
+    </div>
+    <div>
+      <h6>Contact</h6>
+      <p>Valence, 26000, FR</p>
+      <p>
+        <a href="mailto:contact@lesupersite.fr"></a>
+          contact@lesupersite.com
+        </a>
+      </p>
+      <p>06 27 87 45 32</p>
     </div>
   </section>
-  <div>
+  <div class="mentions">
     © 2023 Tous droits réservés :
     <a href="">lesupersite.com</a>
   </div>
