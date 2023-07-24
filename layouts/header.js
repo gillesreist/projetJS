bd9034e11dff76e9f13892e54228bcbb3b23637f
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-07-24 12:04:20 +0200

    menu dropdown fonctionnel

--- layouts/header.js
+++ layouts/header.js
@@ -1,10 +1,26 @@
 const header = document.createElement('header');
 header.innerHTML = `
         <div class="header_container">
+                <div class="header_left_block">
+                </div>
                 <div class="site_name_container">
                         <p>LESUPERSITE</p>
                 </div>
-        </di>
+                <div id="burger_menu">
+                        <span></span>
+                        <div id="menu">
+                                <menu>
+                                        <li><a href="#">accueil</a></li>
+                                        <li><a href="#">galerie</a></li>
+                                        <li><a href="#">jeux</a></li>
+                                </menu>
+                        </div>
+                        
+                </div>
+                
+        </div>
 `;
 document.body.prepend(header);
+
+
 
